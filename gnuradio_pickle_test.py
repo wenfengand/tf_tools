840ff62b30148639f171437c75d6cda2e8c7783c
wenfeng <wenfengand@126.com>
2018-04-11 18:59:34 +0800

    Test input data class

---
+++ gnuradio_pickle_test.py
@@ -0,0 +1,8 @@
+#encoding:utf8
+import gnuradio_pickle
+file_name = '/home/wangwenfeng/dataset/GNUradio/RML2016.10a_dict.dat'
+gnu = gnuradio_pickle.input_data(file_name)
+test_x,test_y = gnu.next_test_batch_snr(0)
+train_x,train_y = gnu.next_train_batch_snr(0)
+print("testx shape is",test_x.shape)
+print("train_x shape is", train_x.shape)
