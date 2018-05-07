510c6e01618c55df355fcd78a556688807e3a27d
wenfeng <wenfengand@126.com>
2018-05-07 11:23:18 +0800

    Bug fixed. In resource function set, select_gpu must be run before enable_memory_growth, or all gpus will be used. Now add a global value to use default gpu, if select gpu appears after enable_memory_growth.

--- resource.py
+++ resource.py
@@ -1,10 +1,12 @@
 import os 
 import tensorflow as tf 
-
+_gpu_number = 0
 def select_gpu(gpu_number):
     # select gpu
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_number)
+    global _gpu_number
+    _gpu_number = gpu_number
 def set_memory_limit(percentage):
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(_gpu_number)
     config = tf.ConfigProto() 
     # 占用显存百分比
     config.gpu_options.per_process_gpu_memory_fraction = percentage
@@ -12,12 +14,15 @@ def set_memory_limit(percentage):
     session.close()
 def enable_memory_growth():
     # 按需占用GPU
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(_gpu_number)
+    print('_gpu_number is', _gpu_number)
     config = tf.ConfigProto()  
     config.gpu_options.allow_growth=True  
     session = tf.Session(config=config)
     session.close()
 def disable_memory_growth():
     # 按需占用GPU
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(_gpu_number)
     config = tf.ConfigProto()  
     config.gpu_options.allow_growth=False   
     session = tf.Session(config=config)
